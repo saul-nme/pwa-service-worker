da84d2edede4ad5bb5c76e8ef90f1dadfa859935
saul-nme <saul@ecohete.com>
2021-10-27 17:02:03 -0500

    first commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener("fetch", (event) => {
+
+  if (event.request.url.includes("style.css")) {
+    event.respondWith(null);
+
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+});
